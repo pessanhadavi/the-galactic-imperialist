6afd0440d6e5d311c9809a52ac9ea58c3b0e431b
David Pessanha <davidppessanha@gmail.com>
2021-07-07 17:50:06 -0300

    Migration was ran

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_07_07_204451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "height"
+    t.string "weight"
+    t.string "dob"
+    t.bigint "planet_id", null: false
+    t.bigint "race_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["planet_id"], name: "index_people_on_planet_id"
+    t.index ["race_id"], name: "index_people_on_race_id"
+  end
+
+  create_table "pilots", force: :cascade do |t|
+    t.bigint "person_id", null: false
+    t.bigint "starcraft_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["person_id"], name: "index_pilots_on_person_id"
+    t.index ["starcraft_id"], name: "index_pilots_on_starcraft_id"
+  end
+
+  create_table "planets", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "races", force: :cascade do |t|
+    t.string "kind"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "starcrafts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "people", "planets"
+  add_foreign_key "people", "races"
+  add_foreign_key "pilots", "people"
+  add_foreign_key "pilots", "starcrafts"
 end
