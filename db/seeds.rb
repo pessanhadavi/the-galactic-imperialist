fab791e364aece72dd4f901b0fdb7dc7419bf3ef
David Pessanha <davidppessanha@gmail.com>
2021-07-07 20:58:47 -0300

    Set the seed of planet's name with some customization

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up database..."
+sleep(1)
+Planet.destroy_all
+puts "Analysing orbits within a massive Jabba-like range..."
+sleep(2)
+puts "Uploading planets' information to the database..."
+puts ""
+
+60.times do |i|
+  planet = SwapiService::Load.get_planets(i+1)
+
+  Planet.create!(
+    name: planet['name']
+  )
+
+  printf("\rProgress: [%-30s]", "=" * (i/2))
+  sleep(0.5)
+end
+
+puts "\nSubtracting planets destroyed by the deathstar..."
+sleep(5)
+puts "Uploaded information of #{Planet.count} planets!"
+
+
+# Person.create!(
+#   name: person['name'],
+#   height: person['height'],
+#   weight: person['weight'],
+#   dob: person['dob']
+# )
+
