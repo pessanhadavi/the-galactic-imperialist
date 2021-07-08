ca381db75b2dccc013f280fb3ccaffa7ee059967
David Pessanha <davidppessanha@gmail.com>
2021-07-08 16:26:18 -0300

    Created method to get the people's information from the API and readjusted the sintax of the other calls for HTTParty usage

--- app/services/swapi_service.rb
+++ app/services/swapi_service.rb
@@ -1,22 +1,19 @@
 require 'open-uri'
 module SwapiService
   class Load
-    def self.get_planets(i)
+    def self.get_planet(i)
       url = "https://swapi.dev/api/planets/#{i}"
-      response = open(url).read
-      parsed_response = JSON.parse(response)
+      response = HTTParty.get(url)
     end
 
     def self.get_species(i)
       url = "https://swapi.dev/api/species/#{i}"
-      response = open(url).read
-      parsed_response = JSON.parse(response)
+      response = HTTParty.get(url)
     end
 
-    def self.get_people(i)
+    def self.get_person(i)
       url = "https://swapi.dev/api/people/#{i}"
-      response = open(url).read
-      parsed_response = JSON.parse(response)
+      response = HTTParty.get(url)
     end
   end
 end
