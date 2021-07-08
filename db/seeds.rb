f0db20a3e58661ba2f4e0555e9aa9ebac1b1e908
David Pessanha <davidppessanha@gmail.com>
2021-07-08 16:26:40 -0300

    Set the seed of people's information with some customization

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
 puts "Cleaning up database..."
 sleep(1)
 # Planet.destroy_all
-Race.destroy_all
+# Race.destroy_all
+Person.destroy_all
 
 # # Uploading planets
 # puts "Analysing orbits within a massive Jabba-like range..."
@@ -11,7 +12,7 @@ Race.destroy_all
 # puts ""
 
 # 60.times do |i|
-#   planet = SwapiService::Load.get_planets(i+1)
+#   planet = SwapiService::Load.get_planet(i+1)
 
 #   Planet.create!(
 #     name: planet['name']
@@ -25,28 +26,68 @@ Race.destroy_all
 # sleep(5)
 # puts "Uploaded information of #{Planet.count} planets!"
 
-# Uploading Races
-37.times do |i|
-  race = SwapiService::Load.get_species(i+1)
+# # Uploading Races
+# puts "Uploading species' information to the database..."
+# sleep(1)
+# puts ""
 
-  Race.create!(
-    kind: race['name']
-  )
+# 37.times do |i|
+#   race = SwapiService::Load.get_species(i+1)
 
-  printf("\rProgress: [%-18s]", "=" * (i/2))
-  sleep(0.5)
+#   Race.create!(
+#     kind: race['name']
+#   )
+
+#   printf("\rProgress: [%-18s]", "=" * (i/2))
+#   sleep(0.5)
+# end
+
+# print "\nSubtracting species extinguished by the Sith Lords..."
+# sleep(5)
+# print "And Luke..."
+# sleep(3)
+# puts "\nUploaded information of #{Race.count} species!"
+
+# Uploading People
+puts "Uploading people information to the database..."
+sleep(1)
+puts ""
+83.times do |i|
+  person = SwapiService::Load.get_person(i + 1)
+  unless person['detail']
+    planet_i = person['homeworld'][/(\d+)/]
+    planet = SwapiService::Load.get_planet(planet_i)
+    person_planet = Planet.find_by name: planet['name']
+    case person['species'].length
+    when 0
+      person_race = Race.first
+    when 1
+      race_i = person['species'].first[/(\d+)/]
+      race = SwapiService::Load.get_species(race_i)
+      person_race = Race.find_by kind: race['name']
+    else
+      race = person['species'].join('-')
+      person_race = Race.create!(
+      kind: race
+      )
+    end
+
+    new_person = Person.new(
+      name: person['name'],
+      height: person['height'],
+      weight: person['mass'],
+      dob: person['birth_year']
+    )
+    new_person.planet = person_planet
+    new_person.race = person_race
+    new_person.save
+  
+    printf("\rProgress: [%-41s]", "=" * (i/2))
+  end
 end
 
-print "\nSubtracting species extinguished by the Sith Lords..."
-sleep(5)
-print "And Luke..."
+print "\nSubtracting Jar Jar Binks..."
 sleep(3)
-puts "\nUploaded information of #{Race.count} species!"
-
-# Person.create!(
-#   name: person['name'],
-#   height: person['height'],
-#   weight: person['weight'],
-#   dob: person['dob']
-# )
-
+print "Kidding..."
+sleep(3)
+puts "\nUploaded information of #{Person.count} people!"
