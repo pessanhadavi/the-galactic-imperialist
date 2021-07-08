b347292418efae337f68b95e43f00c3a7e604e94
David Pessanha <davidppessanha@gmail.com>
2021-07-08 12:05:05 -0300

    Set the seed of specie's name with some customization

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,47 @@
+# Cleaning database
 puts "Cleaning up database..."
 sleep(1)
-Planet.destroy_all
-puts "Analysing orbits within a massive Jabba-like range..."
-sleep(2)
-puts "Uploading planets' information to the database..."
-puts ""
+# Planet.destroy_all
+Race.destroy_all
 
-60.times do |i|
-  planet = SwapiService::Load.get_planets(i+1)
+# # Uploading planets
+# puts "Analysing orbits within a massive Jabba-like range..."
+# sleep(2)
+# puts "Uploading planets' information to the database..."
+# puts ""
 
-  Planet.create!(
-    name: planet['name']
+# 60.times do |i|
+#   planet = SwapiService::Load.get_planets(i+1)
+
+#   Planet.create!(
+#     name: planet['name']
+#   )
+
+#   printf("\rProgress: [%-30s]", "=" * (i/2))
+#   sleep(0.5)
+# end
+
+# puts "\nSubtracting planets destroyed by the deathstar..."
+# sleep(5)
+# puts "Uploaded information of #{Planet.count} planets!"
+
+# Uploading Races
+37.times do |i|
+  race = SwapiService::Load.get_species(i+1)
+
+  Race.create!(
+    kind: race['name']
   )
 
-  printf("\rProgress: [%-30s]", "=" * (i/2))
+  printf("\rProgress: [%-18s]", "=" * (i/2))
   sleep(0.5)
 end
 
-puts "\nSubtracting planets destroyed by the deathstar..."
+print "\nSubtracting species extinguished by the Sith Lords..."
 sleep(5)
-puts "Uploaded information of #{Planet.count} planets!"
-
+print "And Luke..."
+sleep(3)
+puts "\nUploaded information of #{Race.count} species!"
 
 # Person.create!(
 #   name: person['name'],
