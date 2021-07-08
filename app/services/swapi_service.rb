3e171f2561d1ddbe54e948e6c4726c7204d99176
David Pessanha <davidppessanha@gmail.com>
2021-07-08 12:04:39 -0300

    Created method to get the species' information from the API

--- app/services/swapi_service.rb
+++ app/services/swapi_service.rb
@@ -6,5 +6,11 @@ module SwapiService
       response = open(url).read
       parsed_response = JSON.parse(response)
     end
+
+    def self.get_species(i)
+      url = "https://swapi.dev/api/species/#{i}"
+      response = open(url).read
+      parsed_response = JSON.parse(response)
+    end
   end
 end
