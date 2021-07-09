70ee6efc1bea37d69667ed641c16663188da8803
David Pessanha <davidppessanha@gmail.com>
2021-07-08 22:21:22 -0300

    Created method to get the starships' information from the API and readjusted the condition to filter by http status

--- app/services/swapi_service.rb
+++ app/services/swapi_service.rb
@@ -4,16 +4,25 @@ module SwapiService
     def self.get_planet(i)
       url = "https://swapi.dev/api/planets/#{i}"
       response = HTTParty.get(url)
+      response.parsed_response if response.code == 200
     end
 
     def self.get_species(i)
       url = "https://swapi.dev/api/species/#{i}"
       response = HTTParty.get(url)
+      response.parsed_response if response.code == 200
     end
 
     def self.get_person(i)
       url = "https://swapi.dev/api/people/#{i}"
       response = HTTParty.get(url)
+      response.parsed_response if response.code == 200
+    end
+
+    def self.get_starcraft(i)
+      url = "https://swapi.dev/api/starships/#{i}"
+      response = HTTParty.get(url)
+      response.parsed_response if response.code == 200
     end
   end
 end
