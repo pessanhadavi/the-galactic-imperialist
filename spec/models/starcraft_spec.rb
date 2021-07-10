27ae6c69adeb6894e5178eb681a2d91d393b71a5
David Pessanha <davidppessanha@gmail.com>
2021-07-10 19:52:55 -0300

    Using fixtures in Starcraft spec/model

--- spec/models/starcraft_spec.rb
+++ spec/models/starcraft_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Starcraft, :type => :model do
-  subject { described_class.create name: "Anything" }
+  fixtures :starcrafts
+
+  before(:each) do
+    @starcraft = starcrafts(:starcraft_22)
+  end
 
   it "is valid with valid attributes" do
-    expect(subject).to be_valid
+    expect(@starcraft).to be_valid
   end
 
   it "is not valid without a name" do
-    subject.name = nil
-    expect(subject).to_not be_valid
+    @starcraft.name = nil
+    expect(@starcraft).to_not be_valid
   end
 end
