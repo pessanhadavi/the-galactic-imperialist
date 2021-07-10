dfea1daf613365f599990a602704bb4eaa70ce10
David Pessanha <davidppessanha@gmail.com>
2021-07-10 19:48:18 -0300

    Deleted unnecessary @person.save

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -13,49 +13,41 @@ RSpec.describe Person, :type => :model do
 
   it "is not valid without a name" do
     @person.name = nil
-    @person.save
     expect(@person).to_not be_valid
   end
 
   it "is not valid without a height" do
     @person.height = nil
-    @person.save
     expect(@person).to_not be_valid
   end
 
   it "is not valid without a weight" do
     @person.weight = nil
-    @person.save
     expect(@person).to_not be_valid
   end
 
   it "is not valid without a dob" do
     @person.dob = nil
-    @person.save
     expect(@person).to_not be_valid
   end
 
   it "is not valid without a hair color" do
     @person.hair = nil
-    @person.save
     expect(@person).to_not be_valid
   end
 
   it "is not valid without a skin tone" do
     @person.skin = nil
-    @person.save
     expect(@person).to_not be_valid
   end
 
   it "is not valid without an eye color" do
     @person.eye = nil
-    @person.save
     expect(@person).to_not be_valid
   end
 
   it "is not valid without a gender" do
     @person.gender = nil
-    @person.save
     expect(@person).to_not be_valid
   end
 end
