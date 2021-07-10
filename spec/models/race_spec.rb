4a280cc74483e1a640a030a88043f501b875a278
David Pessanha <davidppessanha@gmail.com>
2021-07-10 19:50:54 -0300

    Using fixtures in Race spec/model

--- spec/models/race_spec.rb
+++ spec/models/race_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Race, :type => :model do
-  subject { described_class.create kind: "Any" }
+  fixtures :races
+
+  before(:each) do
+    @race = races(:droid)
+  end
 
   it "is valid with valid attributes" do
-    expect(subject).to be_valid
+    expect(@race).to be_valid
   end
 
   it "is not valid with no kind" do
-    subject.kind = nil
-    expect(subject).to_not be_valid
+    @race.kind = nil
+    expect(@race).to_not be_valid
   end
 end
