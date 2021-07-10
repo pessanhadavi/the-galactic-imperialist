5e158d9b0242ef5bfe488652f51578e209d0bc2e
David Pessanha <davidppessanha@gmail.com>
2021-07-10 19:11:22 -0300

    Modiefied the tests to use fixtures + created the test for seeds/aux_methods: people_and_pilots' #pilot_license_registration method

--- spec/seeds/people_and_pilots_spec.rb
+++ spec/seeds/people_and_pilots_spec.rb
@@ -2,28 +2,32 @@ require 'rails_helper'
 require_relative '../../db/seeds/aux_methods/people_and_pilots.rb'
 
 describe "Seeds/aux_methods: people_and_pilots" do
+  fixtures :planets, :races, :starcrafts, :people
+  
+  binding.pry
+  
   describe "#find_planet" do
-    planet = Planet.create name: "Tatooine"
 
     it "returns the right planet instance" do
+      tatooine = planets(:tatooine)
       planet_found = find_planet("https://swapi.dev/api/planets/1")
-      expect(planet_found.name).to eq(planet.name)
+      expect(planet_found.name).to eq(tatooine.name)
     end
   end
 
   describe "#find_race" do
-    race = Race.create kind: "Human"
-    race_2 = Race.create kind: "Droid"
-    race_3 = Race.create kind: "Droid-Wookie"
 
     it "returns the right race instances" do
-      race_found = find_race([])
-      race_2_found = find_race(["https://swapi.dev/api/species/2/"])
-      race_3_found = find_race(["https://swapi.dev/api/species/2/", "https://swapi.dev/api/species/3/"])
+      human = races(:human)
+      droid = races(:droid)
+      droid_wookie = races(:droid_wookie)
+      human_found = find_race([])
+      droid_found = find_race(["https://swapi.dev/api/species/2/"])
+      droid_w_found = find_race(["https://swapi.dev/api/species/2/", "https://swapi.dev/api/species/3/"])
 
-      expect(race_found.kind).to eq(race.kind)
-      expect(race_2_found.kind).to eq(race_2.kind)
-      expect(race_3_found.kind).to eq(race_3.kind)
+      expect(human_found.kind).to eq(human.kind)
+      expect(droid_found.kind).to eq(droid.kind)
+      expect(droid_w_found.kind).to eq(droid_wookie.kind)
     end
   end
 
@@ -34,6 +38,16 @@ describe "Seeds/aux_methods: people_and_pilots" do
 
       expect(starships).to eq(false)
       expect(starships_1).to eq(true)
+    end
+  end
+
+  describe "#pilot_license_registration" do
+    it "creates pilot instances" do
+      starships_api = ["https://swapi.dev/api/starships/12/", "https://swapi.dev/api/starships/22/"]
+      person = people(:michael)
+
+      pilot_license_registration(starships_api, person)
+      expect(person.starcrafts.length > 0).to eq(true)
     end
   end
 end
