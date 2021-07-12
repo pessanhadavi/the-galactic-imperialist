89a7ffe67de829c156bb8def8f4e8a453f09b923
David Pessanha <davidppessanha@gmail.com>
2021-07-12 01:05:01 -0300

    Created show method on controller and rendered a js format

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,11 @@ class PeopleController < ApplicationController
   def index
     @people = Person.all
   end
+
+  def show
+    @person = Person.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
 end
