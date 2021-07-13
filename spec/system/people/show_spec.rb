a173286cbb11c2a3c2d2ecff077a8d18d4dc372e
David Pessanha <davidppessanha@gmail.com>
2021-07-13 18:58:59 -0300

    Created system test for show page by clicking

--- spec/system/people/show_spec.rb
+++ spec/system/people/show_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Show person', type: :system do
+  scenario 'showing character' do
+    visit root_path
+    click_on 'C-3PO'
+
+    expect(page).to have_content('My name is C-3PO')
+  end
+
   scenario 'searching character' do
     visit root_path
     fill_in '#search-inp', with: 'Leia'
     click_button '#search-btn'
+    click_on 'Leia Organa'
 
     expect(page).to have_content('My name is Leia Organa')
   end
